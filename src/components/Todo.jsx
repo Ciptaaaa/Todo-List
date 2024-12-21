d613b3c4465ca7918e405f3afc749c6366d890c1
Cipta <ordinarypeople67@gmail.com>
2024-12-21 15:47:25 +0700

    Feat: add owner n position, fixing responsive tablet

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,6 +3,10 @@ import todo_icon from "../assets/todo_icon.png";
 import Todoitems from "./Todoitems";
 
 const Todo = () => {
+  const user = {
+    name: "John Doe",
+    position: "FE Hari Senin.com",
+  };
   //untuk menyimpan todo list agar tidak menghilang saat website di refresh maupun di close
   const [todoList, setTodoList] = useState(
     localStorage.getItem("todos")
@@ -69,24 +73,37 @@ const Todo = () => {
   return (
     <div className="bg-white place-self-center w-11/12 sm:w-10/12 md:w-8/12 lg:w-6/12 max-w-full flex flex-col p-4 sm:p-6 md:p-8 min-h-screen rounded-xl">
       {/* Title */}
-      <div className="flex items-center mt-4 sm:mt-6 gap-2">
-        <img className="w-8" src={todo_icon} alt="" />
-        <h1 className="text-xl sm:text-2xl md:text-3xl font-semibold">
-          To-Do-List
-        </h1>
+      <div className="flex items-center mt-4 sm:mt-6 gap-2 justify-between ">
+        <div className="flex items-center gap-2 flex-grow">
+          <img className="w-8" src={todo_icon} alt="" />
+          <h1 className="text-xl sm:text-2xl md:text-3xl font-semibold items-start ">
+            To-Do-List
+          </h1>
+        </div>
+
+        <div className="mt-3 text-sm text-gray-700 ">
+          <p className="flex justify-between items-center">
+            <span className="mr-4">
+              <strong>Owner:</strong> {user.name}
+            </span>
+            <span className="ml-auto">
+              <strong>Position:</strong> {user.position}
+            </span>
+          </p>
+        </div>
       </div>
 
       {/* Input Box */}
-      <div className="flex flex-col sm:flex-row items-center gap-4 my-6 sm:my-8 bg-gray-200 ">
+      <div className="flex relative flex-col sm:flex-row items-center gap-2 my-6 sm:my-8 bg-gray-200 rounded-xl ">
         <input
           ref={inputRef}
-          className="bg-transparent border-0 outline-none flex-1 h-12 sm:h-14 pl-4 sm:pl-6 pr-2 placeholder:text-slate-600 mb-4 sm:mb-0 w-full"
+          className="bg-transparent border-0 outline-none flex-1 h-12 sm:h-14 px-4 sm:pl-6 pr-2 placeholder:text-slate-600 mb-2 sm:mb-0 w-full sm:w-auto "
           type="text"
           placeholder="Add your task"
         />
         <select
           ref={categoryRef}
-          className="bg-gray-200 border-0 rounded-full h-12 sm:h-14 px-4 outline-none text-slate-600 mb-4 sm:mb-0 w-full sm:w-auto"
+          className="bg-gray-200 border-0  h-12 sm:h-14 px-4 outline-none text-slate-600 mb-2 sm:mb-0 w-full sm:w-auto"
           defaultValue=""
         >
           <option value="" disabled>
@@ -96,7 +113,10 @@ const Todo = () => {
           <option value="Medium">Medium</option>
           <option value="High">High</option>
         </select>
-        <button class="border-none  bg-orange-600 w-full sm:w-32 h-12 sm:h-14 text-white text-sm sm:text-lg font-medium cursor-pointer mt-4 sm:mt-0 flex justify-center items-center">
+        <button
+          onClick={add}
+          className="border-none  bg-orange-600 w-full sm:w-32 h-12 sm:h-14 text-white text-sm sm:text-lg font-medium cursor-pointer sm:mt-0 flex justify-center items-center rounded-xl"
+        >
           Add task!
         </button>
       </div>
@@ -114,6 +134,8 @@ const Todo = () => {
               toggle={toggle}
               timestamp={item.timestamp}
               category={item.category}
+              name={user.name}
+              position={user.position}
             />
           );
         })}
